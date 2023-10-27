437b17e7058ec2040f401d163259c4150f4b075a
Silent Code <a.silentcode@gmail.com>
2023-10-28 05:29:51 +0600

    Mobile nav, banner refactored

--- src/data/nav-links.js
+++ src/data/nav-links.js
@@ -1,36 +1,50 @@
-export const headerLinks = [
-    {
-        href: '/',
-        text: 'Home',
-    },
-    {
-        href: '#',
-        text: 'Pages',
-        subPages: [
-            {
-                href: "#",
-                text: 'Blog',
-            },
-            {
-                href: "#",
-                text: 'Blog Single',
-            },
-            {
-                href: "#",
-                text: 'Product',
-            },
-        ],
-    },
-    {
-        href: '/about',
-        text: 'About',
-    },
-    {
-        href: '/',
-        text: 'Services',
-    },
-    {
-        href: '/',
-        text: 'Contact',
-    },
-]
+export const primaryLinks = [
+  {
+    href: "/",
+    text: "Home",
+  },
+  {
+    href: "#",
+    text: "Pages",
+    subPages: [
+      {
+        href: "#",
+        text: "Blog",
+      },
+      {
+        href: "#",
+        text: "Blog Single",
+      },
+      {
+        href: "#",
+        text: "Product",
+      },
+    ],
+  },
+  {
+    href: "#",
+    text: "About",
+    subPages: [
+      {
+        href: "#",
+        text: "Mission",
+      },
+      {
+        href: "#",
+        text: "Goal",
+      },
+      {
+        href: "#",
+        text: "Careers",
+      },
+    ],
+  },
+  {
+    href: "/",
+    text: "Services",
+  },
+  {
+    href: "/style-guide",
+    text: "Styles",
+  },
+];
