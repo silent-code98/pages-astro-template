aa54687b85da47fc5a3af1cd10f52d9d309cd262
Silent Code <a.silentcode@gmail.com>
2023-10-29 19:51:19 +0600

    Minor fixes

--- src/data/books.js
+++ src/data/books.js
@@ -3,10 +3,10 @@ export const featuredBooks = [
     id: "1",
     name: "Atomic One’s",
     desc: "Many variations of passages of Lorem Ipsum willing araise  alteration in some form.",
-    pages: "586",
+    pages: "367",
     length: "10",
     rating: "3.5",
-    image: "/images/hero-book.jpg",
+    image: "/images/book-2.jpg",
   },
   {
     id: "2",
@@ -15,6 +15,6 @@ export const featuredBooks = [
     pages: "586",
     length: "6",
     rating: "3.5",
-    image: "/images/book-2.jpg",
+    image: "/images/hero-book.jpg",
   },
 ];
