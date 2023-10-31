fe34e0dab1d34344ced7268ea322f0a804eeaf3d
Silent Code <a.silentcode@gmail.com>
2023-10-31 19:08:13 +0600

    3 new sections added - homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#1B3764",
-        secondary: "#FFCA42",
-        "light-blue": "#B4C7E7",
-      },
-      container: {
-        center: true,
-      },
-    },
-    fontFamily: {
-      inter: ["Inter", "ui-sans-serif", "system-ui"],
-      cardo: ["Cardo", "ui-serif", "Georgia"],
-    },
-  },
-  plugins: [],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#1B3764',
+				secondary: '#FFCA42',
+				'light-blue': '#B4C7E7'
+			},
+			container: {
+				center: true
+			},
+			boxShadow: {
+				light: '0px 35px 25px 0px rgba(4, 11, 20, 0.04)'
+			}
+		},
+		fontFamily: {
+			inter: ['Inter', 'ui-sans-serif', 'system-ui'],
+			cardo: ['Cardo', 'ui-serif', 'Georgia']
+		}
+	},
+	plugins: []
+}
