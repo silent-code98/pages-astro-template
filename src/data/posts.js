9358d9c2e3b73a0ad0769fb717dfe36c085b8e94
Silent Code <a.silentcode@gmail.com>
2023-11-01 19:41:17 +0600

    Artcles section added

---
+++ src/data/posts.js
@@ -0,0 +1,29 @@
+export const posts = [
+	{
+		id: '1',
+		title: 'The energy efficiency offers hydrotherapy or swim',
+		excerpt:
+			'The point of using Lorem Ipsum hiter of that using making it look like others readable will get end.',
+		author: 'Desk',
+		date: '23.05.2024',
+		image: '/images/article-1.jpg'
+	},
+	{
+		id: '2',
+		title: 'Release of Letraset sheets tools containing  passages',
+		excerpt:
+			'The point of using Lorem Ipsum hiter of that using making it look like others readable will get end.',
+		author: 'Desk',
+		date: '23.05.2024',
+		image: '/images/article-2.jpg'
+	},
+	{
+		id: '3',
+		title: 'The energy efficiency offers hydrotherapy or swim',
+		excerpt:
+			'The point of using Lorem Ipsum hiter of that using making it look like others readable will get end.',
+		author: 'Desk',
+		date: '23.05.2024',
+		image: '/images/article-3.jpg'
+	}
+]
