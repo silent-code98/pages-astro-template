708d0ae2ae62fa1514c486e971c02316d6237ab7
Silent Code <a.silentcode@gmail.com>
2023-11-02 21:50:55 +0600

    Footer section added

--- src/data/nav-links.js
+++ src/data/nav-links.js
@@ -1,50 +1,104 @@
 export const primaryLinks = [
-  {
-    href: "/",
-    text: "Home",
-  },
-  {
-    href: "#",
-    text: "Pages",
-    subPages: [
-      {
-        href: "#",
-        text: "Blog",
-      },
-      {
-        href: "#",
-        text: "Blog Single",
-      },
-      {
-        href: "#",
-        text: "Product",
-      },
-    ],
-  },
-  {
-    href: "#",
-    text: "About",
-    subPages: [
-      {
-        href: "#",
-        text: "Mission",
-      },
-      {
-        href: "#",
-        text: "Goal",
-      },
-      {
-        href: "#",
-        text: "Careers",
-      },
-    ],
-  },
-  {
-    href: "/",
-    text: "Services",
-  },
-  {
-    href: "/style-guide",
-    text: "Styles",
-  },
-];
+	{
+		href: '/',
+		text: 'Home'
+	},
+	{
+		href: '#',
+		text: 'Pages',
+		subPages: [
+			{
+				href: '#',
+				text: 'Blog'
+			},
+			{
+				href: '#',
+				text: 'Blog Single'
+			},
+			{
+				href: '#',
+				text: 'Product'
+			}
+		]
+	},
+	{
+		href: '#',
+		text: 'About',
+		subPages: [
+			{
+				href: '#',
+				text: 'Mission'
+			},
+			{
+				href: '#',
+				text: 'Goal'
+			},
+			{
+				href: '#',
+				text: 'Careers'
+			}
+		]
+	},
+	{
+		href: '/',
+		text: 'Services'
+	},
+	{
+		href: '/style-guide',
+		text: 'Styles'
+	}
+]
+
+export const footerExplore = [
+	{
+		href: '/',
+		text: 'Home'
+	},
+	{
+		href: '/',
+		text: 'About Us'
+	},
+	{
+		href: '/',
+		text: ' Services'
+	},
+	{
+		href: '/',
+		text: 'Appointments'
+	},
+	{
+		href: '/',
+		text: 'Blog'
+	},
+	{
+		href: '/',
+		text: 'Contact Us'
+	}
+]
+
+export const footerUtilityPages = [
+	{
+		href: '/',
+		text: 'Start here'
+	},
+	{
+		href: '/',
+		text: 'Style guide'
+	},
+	{
+		href: '/',
+		text: '404 not found'
+	},
+	{
+		href: '/',
+		text: 'Password protected'
+	},
+	{
+		href: '/',
+		text: 'Licenses'
+	},
+	{
+		href: '/',
+		text: 'Changelog'
+	}
+]
