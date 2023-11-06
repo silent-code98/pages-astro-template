2d258a33b54e9fd5d5577deb29cf3e9423f806cc
Silent Code <a.silentcode@gmail.com>
2023-11-06 22:31:54 +0600

    about page completed

--- src/data/books.js
+++ src/data/books.js
@@ -1,20 +1,71 @@
 export const featuredBooks = [
-  {
-    id: "1",
-    name: "Atomic One’s",
-    desc: "Many variations of passages of Lorem Ipsum willing araise  alteration in some form.",
-    pages: "367",
-    length: "10",
-    rating: "3.5",
-    image: "/images/book-2.jpg",
-  },
-  {
-    id: "2",
-    name: "The Dark Light",
-    desc: "Many variations of passages of Lorem Ipsum willing araise  alteration in some form.",
-    pages: "586",
-    length: "6",
-    rating: "3.5",
-    image: "/images/hero-book.jpg",
-  },
-];
+	{
+		id: '1',
+		name: 'Atomic One’s',
+		desc: 'Many variations of passages of Lorem Ipsum willing araise  alteration in some form.',
+		pages: '367',
+		length: '10',
+		rating: '3.5',
+		image: '/images/book-2.jpg'
+	},
+	{
+		id: '2',
+		name: 'The Dark Light',
+		desc: 'Many variations of passages of Lorem Ipsum willing araise  alteration in some form.',
+		pages: '586',
+		length: '6',
+		rating: '3.5',
+		image: '/images/hero-book.jpg'
+	}
+]
+
+export const storeListings = [
+	{
+		image: '/images/hero-book.jpg',
+		title: 'Atomic Ones',
+		desc: 'Many variations of passages of Lorem Ipsum willing araise  alteration in some form.',
+		price: 23.89,
+		bookType: 'Printed Book',
+		url: '/'
+	},
+	{
+		image: '/images/book-2.jpg',
+		title: 'Atomic Ones',
+		desc: 'Many variations of passages of Lorem Ipsum willing araise  alteration in some form.',
+		price: 23.89,
+		bookType: 'Printed Book',
+		url: '/'
+	},
+	{
+		image: '/images/hero-book.jpg',
+		title: 'Atomic Ones',
+		desc: 'Many variations of passages of Lorem Ipsum willing araise  alteration in some form.',
+		price: 23.89,
+		bookType: 'eBook',
+		url: '/'
+	},
+	{
+		image: '/images/book-2.jpg',
+		title: 'Atomic Ones',
+		desc: 'Many variations of passages of Lorem Ipsum willing araise  alteration in some form.',
+		price: 23.89,
+		bookType: 'Audio Book',
+		url: '/'
+	},
+	{
+		image: '/images/hero-book.jpg',
+		title: 'Atomic Ones',
+		desc: 'Many variations of passages of Lorem Ipsum willing araise  alteration in some form.',
+		price: 23.89,
+		bookType: 'Printed Book',
+		url: '/'
+	},
+	{
+		image: '/images/book-2.jpg',
+		title: 'Atomic Ones',
+		desc: 'Many variations of passages of Lorem Ipsum willing araise  alteration in some form.',
+		price: 23.89,
+		bookType: 'eBook',
+		url: '/'
+	}
+]
